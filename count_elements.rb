ddb1e39dab7490779c92439011a7d7ed55a99d13
github username <github email address>
2018-10-30 15:54:25 -0400

    Done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
   # code goes here
+  new_hash = {}
+  array.each do |animal|
+    if new_hash.include?(animal)
+      new_hash[animal] += 1
+    else
+      new_hash[animal] = 1
+    end
+  end
+  new_hash
 end
- 
